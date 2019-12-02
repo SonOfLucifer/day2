8ccde6c4496df62477479b42d2668248143621a5
SonOfLucifer <32776705+SonOfLucifer@users.noreply.github.com>
2019-12-02 15:47:19 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,15 +3,15 @@
 #include <algorithm>
 #include <math.h>
 #include <fstream>
+#include <vector>
 using namespace std;
 ifstream f("in.txt");
-int v[1000];
-/*
-void function2(int a, int l, int ok){
-    int i;
+
+int func(vector<long> v, long a, long b, long ok){
+    long i;
     v[1] = a;
-    v[2] = l;
-    for(int i = 0;i<ok;i+=4)
+    v[2] = b;
+    for(i=0;i<v.size();i+=4){
         if(v[i] == 1)
             v[v[i+3]] = v[v[i+2]] + v[v[i+1]];
         else
@@ -20,44 +20,22 @@ void function2(int a, int l, int ok){
         else
             if(v[i]==99)
                 i++;
-}*/
-int main()
-{
-    char b[1000], *p;
-    int ok = 0, i, j, k, ii, jj, l = 0;
+    }
+    return v[0];
+    
+}
+int main(){
+    vector<long> v;
+    char b[10000], *p;
+    int i, j, ok = 0;
     cin.getline(b, sizeof(b));
-    //19690720
     p = strtok(b, ",");
-    while(p)
-    {
-        v[ok++] = atoi(p);
+    while(p){
+        v.push_back(atol(p));
         p = strtok(NULL, ",");
     }
-    for(k=99; k>=0; k--){
-        for(j=0; j<=99; j++)
-        {
-            v[1] = k;
-            v[2] = j;
-            for(int i = 0; i<ok; i+=4)
-                if(v[i] == 1)
-                    v[v[i+3]] = v[v[i+2]] + v[v[i+1]];
-                else if(v[i] == 2)
-                    v[v[i+3]] = v[v[i+2]] * v[v[i+1]];
-                else if(v[i]==99)
-                    i++;
-            if(v[0] == 19690720)
-            {
-                ii=i;
-                jj = j;
-                l = 1;
-                break;
-            }
-        }
-        if(l==1)
-            break;
-    }
-    cout<<100*ii+jj;
-
+    for(i=99;i>=0;i--)
+        for(j=0;j<=99;j++)
+            if(func(v, i, j, ok) == 19690720)
+                cout<<100*i+j;
 }
-
-
